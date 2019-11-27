585cc653fd700f124f170acca5bd8ed825c227c9
boomin <akachris225@gmail.com>
2019-11-27 21:28:01 +0000

    Menu and Contact basic interface

--- app/src/main/java/com/example/firstandroidtp/MainActivity.java
+++ app/src/main/java/com/example/firstandroidtp/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.firstandroidtp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button contactButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        contactButton = (Button) findViewById(R.id.contactButton);
+        contactButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent contactActivity = new Intent(getApplicationContext(), ContactActivity.class);
+                startActivity(contactActivity);
+            }
+        });
     }
 }
