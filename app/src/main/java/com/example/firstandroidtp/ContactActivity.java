7820b61cfa78c0c33ce8f92f8ccd084a17c63ee1
eliud2000 <etiemele01@gmail.com>
2019-11-28 13:31:08 +0000

    enregistrement d'un contact

--- app/src/main/java/com/example/firstandroidtp/ContactActivity.java
+++ app/src/main/java/com/example/firstandroidtp/ContactActivity.java
@@ -8,12 +8,14 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class ContactActivity extends AppCompatActivity {
 
     private Button validerButton;
     private EditText nom;
     private EditText numero;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,21 +25,28 @@ public class ContactActivity extends AppCompatActivity {
         validerButton = (Button) findViewById(R.id.save);
         nom = (EditText) findViewById(R.id.nom);
         numero = (EditText) findViewById(R.id.numero);
-
+        textView=(TextView) findViewById(R.id.resutas);
         validerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                Log.d("Nom", nom.getText().toString());
-                Log.d("Numero", numero.getText().toString());
+                save();
+                load();
             }
         });
-
+    load();
     }
     private void save() {
         SharedPreferences sharedPreferences = getSharedPreferences("shared preference", MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString("eliud","ok");
-        
+        editor.putString("Nom","eliud");
+        editor.putString("Numero","01020304");
+        editor.apply();
+
+    }
+    private void load(){
+        SharedPreferences sharedPreferences=getSharedPreferences("shared preference",MODE_PRIVATE);
+        String res1=sharedPreferences.getString("Nom","");
+        String res2=sharedPreferences.getString("Numero","");
+        textView.setText("nom : "+res1+"\n"+"prenom : "+res2+"\n");
     }
 }
