7d955e297831fef1ca952d607716af7d1dbf078e
eliud2000 <etiemele01@gmail.com>
2019-11-28 13:10:30 +0000

    ceation du sharePreference

--- app/src/main/java/com/example/firstandroidtp/ContactActivity.java
+++ app/src/main/java/com/example/firstandroidtp/ContactActivity.java
@@ -2,6 +2,7 @@ package com.example.firstandroidtp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -26,9 +27,17 @@ public class ContactActivity extends AppCompatActivity {
         validerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 Log.d("Nom", nom.getText().toString());
                 Log.d("Numero", numero.getText().toString());
             }
         });
+
+    }
+    private void save() {
+        SharedPreferences sharedPreferences = getSharedPreferences("shared preference", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("eliud","ok");
+        
     }
 }
