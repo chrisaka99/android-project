30948a46160d3fad5aec697d550e326ddedc6d2c
dykoffi <koffiedy@gmail.com>
2019-11-28 05:54:35 +0000

    implementation de la fonction de saisie et de suppression à l'ecran

--- app/src/main/java/com/example/firstandroidtp/MainActivity.java
+++ app/src/main/java/com/example/firstandroidtp/MainActivity.java
@@ -3,12 +3,45 @@ package com.example.firstandroidtp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText ecranResultat = null;
+    private EditText ecranCalcul = null;
+    private Button un = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        un = (Button) findViewById(R.id.un);
+        ecranResultat = (EditText) findViewById(R.id.ecranResultat);
+        ecranCalcul = (EditText) findViewById(R.id.ecranCalcul);
+
+
+
     }
+
+    public void saisirCalcul(View v){
+        ecranCalcul.getText().append(((Button) v).getText());
+    }
+
+    public void cleanAll(View v){
+        ecranCalcul.getText().clear();
+        ecranResultat.getText().clear();
+    }
+
+    public  void del(View v) {
+        int t = ecranCalcul.getText().length();
+        if ( t > 0) {
+            ecranCalcul.getText().delete(t-1, t);
+        }
+    }
+
+
 }
