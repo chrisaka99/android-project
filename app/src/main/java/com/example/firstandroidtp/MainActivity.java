18a170450433a9daa7b52cbd80450b50a04f7703
eliud2000 <etiemele01@gmail.com>
2019-11-30 05:37:36 +0000

    changement de class chronometre et fin

--- app/src/main/java/com/example/firstandroidtp/MainActivity.java
+++ app/src/main/java/com/example/firstandroidtp/MainActivity.java
@@ -4,50 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Chronometer;
-import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button contactButton;
-
-    private Button validerButton;
-    private EditText nom;
-    private EditText numero;
-    private TextView textView;
-    private contactBDD ContactBDD;
-    private Chronometer chrono;
-    private  boolean running;
-    private  long pauseOffset;
+    private Button chronoButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_chrono);
-        chrono=(Chronometer) findViewById(R.id.chronometer);
-    }
-    public void startChronometer(View v){
-        if(!running){
-            chrono.setBase(SystemClock.elapsedRealtime()-pauseOffset);
-            chrono.start();
-            running=true;
-        }
-    }
-    public void pauseChronometer(View v){
-        if(running){
-            chrono.stop();
-            pauseOffset=SystemClock.elapsedRealtime()-chrono.getBase();
-            running=false;
-        }
-    }
-    public void resetChronometer(View v){
-        chrono.setBase(SystemClock.elapsedRealtime());
-        pauseOffset=0;
-    }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+        chronoButton = (Button) findViewById(R.id.chronoButton);
+        chronoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /* on lance la nouvelle activite */
+                Intent chronoActivity = new Intent(getApplicationContext(), chronoActivity.class);
+                startActivity(chronoActivity);
+            }
+        });
+    }
 }
-
